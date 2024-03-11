3341bbba6f8c75207f5cb3c425eff6ec05d7a75e
HweyTH <thhuy1101@gmail.com>
2024-03-10 23:01:25 -0400

    Third Commit

--- model/main.py
+++ model/main.py
@@ -11,25 +11,36 @@ api_key = os.getenv("COHERE_API_KEY")
 co = cohere.Client(api_key)
 
 # load user query json
-with open('example_user_queries.json') as user_file:
+with open('features.json') as user_file:
     user_input = user_file.read()
 # load tenant posting data json
-with open('example_posting.json') as data_file:
+with open('houses_data.json') as data_file:
     houses_data = data_file.read()
 
-# parse json data sets
+# parse json to python dictionaries
 user_query = orjson.loads(user_input)
 houses = orjson.loads(houses_data)
 
+# remove images field from the data input
+house_queries = []
+for query in houses:
+    new_query = {}
+    for key in query.keys():
+        if key in user_query.keys() or key == 'description':
+            new_query.update({key: query.get(key)})
+    house_queries.append(new_query)
+
+# convert rental listing data to one string
 docs = []
-for house in houses:
-    docs.append(str(house))
+for query in house_queries:
+    docs.append(str(query))
 
-user_input = str(user_query[0])
+# convert user input to one string
+user_input = str(user_query)
 
-results = co.rerank(query=user_input, documents=docs, top_n=3, model='rerank-english-v2.0')
+result = co.rerank(query=user_input, documents=docs, top_n=5, model='rerank-multilingual-v2.0')
 
-for idx, r in enumerate(results):
+for idx, r in enumerate(result):
     print(f"Document Rank: {idx + 1}, Document Index: {r.index}")
     print(f"Document: {r.document['text']}")
     print(f"Relevance Score: {r.relevance_score:.2f}")
